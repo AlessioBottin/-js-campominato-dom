58c1841c3e858e8acff44472f6adcf52ae961bce
AlessioBottin <alessiobottin2002@gmail.com>
2021-11-24 17:01:31 +0100

    STARTED HANDLECELLCLICK

--- js/script.js
+++ js/script.js
@@ -1,15 +1,53 @@
+// PARTE 1 
 // L'utente indica un livello di difficoltà in base al quale viene generata una griglia di gioco quadrata, in cui ogni cella contiene un numero tra quelli compresi in un range:
 // con difficoltà 1 => tra 1 e 100
 // con difficoltà 2 => tra 1 e 81
 // con difficoltà 3 => tra 1 e 49
 // Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro.
 
+
+// // PARTE 2 
+// Copiamo la griglia fatta ieri nella nuova repo e aggiungiamo la logica del gioco (attenzione: 
+// non bisogna copiare tutta la cartella dell'esercizio ma solo l'index.html, e le cartelle js/ css/ con i relativi script e fogli di stile, per evitare problemi 
+// con l'inizializzazione di git).
+// L'utente indica un livello di difficoltà in base al quale viene generata una griglia di gioco quadrata, in cui ogni cella contiene un numero tra quelli compresi in un range:
+// con difficoltà 1 => tra 1 e 100
+// con difficoltà 2 => tra 1 e 81
+// con difficoltà 3 => tra 1 e 49
+// Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe.
+// I numeri nella lista delle bombe non possono essere duplicati.
+// In seguito l'utente clicca su una cella:
+// se il numero è presente nella lista dei numeri generati - abbiamo calpestato una bomba - la cella si colora di rosso e la partita termina, 
+// altrimenti la cella cliccata si colora di azzurro e l'utente può continuare a cliccare sulle altre celle.
+// La partita termina quando il giocatore clicca su una bomba o raggiunge il numero massimo possibile di numeri consentiti.
+// Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha cliccato su una cella che non era una b.
+// BONUS:
+// 1- quando si clicca su una bomba e finisce la partita, evitare che si possa cliccare su altre celle
+// 2- quando si clicca su una bomba e finisce la partita, il software scopre tutte le bombe nascoste
+
+
+// ANALISI 
+// [x] imposto il numero di bombe generatedBombsNumber
+// [x] imposto il numero di maxRightAttempts 
+// [x] Genero l'array di 16 numeri casuali e diversi compresi tra 1 e generatedCellsNumber: le bombe 
+// [] al click:
+//              [] se il numero cliccato è presente nella lista delle bombsArray
+//                 - [] do alla cella classe bomb (colore rosso) 
+//                 - [] termino la partita con una funzione endGame
+//              [] altrimenti la cella si colora di azzurro e la cella non è più cliccabile
+//                 - [] inserisco il numero in un array di rightAttempts
+
+//   se la lunghezza di rightAttempts = generatedCellsNumber - genereatedBombsNumber (16) (maxRightAttempts)
+//     - [] termina la partita con una funzione endGame
+
+
+
 // Quando l' utente cliccca play parte il gioco
 const playBtn = document.getElementById('play-btn');
 playBtn.addEventListener('click', gameStart);
 
 function gameStart() {
-    // Raccolgo il dato sull' impostazione della difficolta 
+    // Raccolgo il dato sull'impostazione della difficolta 
     const gameDifficulty = document.getElementById('difficulty').value;
     let generatedCellsNumber;
 
@@ -17,6 +55,11 @@ function gameStart() {
     const grid = document.getElementById('grid');
     grid.innerHTML = '';
 
+    // imposto il numero di bombe 
+    let generatedBombsNumber = 16;
+
+    // imposto il numero di maxRightAttempts 
+    let maxRightAttempts = generatedCellsNumber - generatedBombsNumber;
     
     // Cambio il numero di celle in base alla difficoltà 
     switch (gameDifficulty) {
@@ -35,6 +78,17 @@ function gameStart() {
     // Genero l' array di numeri casuali e diversi 
     const rndNumbersArray = getRndNumbersArray(generatedCellsNumber);
 
+    // Genero l'array di 16 numeri casuali e diversi compresi tra 1 e generatedCellsNumber: le bombe 
+    const bombsArray = [];
+
+    while ( bombsArray.length < generatedBombsNumber ) {
+        let genereatedBomb = getRndInteger(1, generatedCellsNumber);
+
+        if ( !bombsArray.includes(genereatedBomb) ) {
+            bombsArray.push(genereatedBomb);
+        }
+    }
+
     // Per ogni numero genero una cella e la appendo al div che la contiene
     for ( i = 0; i < rndNumbersArray.length; i++ ) {
         thisNumber = rndNumbersArray[i];
@@ -42,25 +96,26 @@ function gameStart() {
         // Genero cella 
         const createdCell = createCell(thisNumber, generatedCellsNumber);
 
-        // Aggiungo la classe clicked
+        // Do alla cella la possibilità di essere cliccata:
         createdCell.addEventListener('click', handleCellClick);
 
         // Appendo cella 
         grid.appendChild(createdCell);
+    }   
+   
+    
+    // DOM SPECIFIC FUNCTIONS 
+    // al click:
+    function handleCellClick() {
+        // se il numero cliccato è presente nella lista delle bombsArray
+        const clickedNumber = parseInt(this.querySelector('span').textContent);
+   
+        if ( bombsArray.includes(clickedNumber) ) {
+        // do alla cella classe bomb (colore rosso) 
+        this.classList.add('bomb');
     }
-
 }
-
-// test 
-
-
-// ---------
-// DOM FUNCTIONS 
-// ---------
-
-function handleCellClick() {
-    this.classList.add('clicked');
-}
+} // ATTENZIONE DA CHIEDERE AD ALE O OLGA: devo aggiungere questa grafa se no dà unexpected end of input a fine programma 
 
 
 // ---------
